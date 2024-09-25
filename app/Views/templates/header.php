c242fc45ef652e5845b7d9f46b94b16f956ed06b
Fl4m3urDev <VoxFlameur22@gmail.com>
2024-09-25 10:31:46 +0200

    add branch louis & arnold + switched front on branch louis

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -6,13 +6,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" type="image/x-icon" href="<?php echo base_url() . 'assets/images/favicon.ico' ?>">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
-    <nav class="navbar navbar-dark bg-dark col-12" id="nav">
+    <nav class="navbar bg-danger col-12" id="nav">
         <div class="justify-content-start d-flex flex-nowrap align-items-center col">
-            <a class="navbar-brand justify-content-start pt-2" href="<?php echo site_url('Visiteur/accueil') ?>">
-                <img class="d-inline-block ms-2" width=54 height=50 src="<?php echo base_url() . '/assets/images/logo.jpg' ?>" alt="Logo">
+            <a class="navbar-brand justify-content-start text-white ms-2 pt-2" href="<?php echo site_url('Visiteur/accueil') ?>">
+                TurboCholestérol
             </a>
         </div>
     </nav>
--- app/Views/visiteur/accueil.php
+++ app/Views/visiteur/accueil.php
@@ -1,6 +1,11 @@
 <div class="container-fluid">
-  <h1 class='mt-2'>Accueil</h1>
+  <h1 class='mt-2 text-center'>Accueil</h1>
   <hr />
-  <div class="row">
+  <div class="d-flex justify-content-evenly">
+    <button class="btn btn-primary">Insérer une activité</button></a></td>
+    <button class="btn btn-primary">Insérer de la nourriture</button></a></td>
+  </div>
+  <div class="d-flex justify-content-center">
+    <button class="btn btn-primary">Découvre le classement</button></a></td>
   </div>
 </div>
