ca73b74c8a44b6e2b3ed4274e7620f4ca927cba0
Fl4m3urDev <VoxFlameur22@gmail.com>
2024-09-25 16:07:39 +0200

    fix formulaire_activite

--- app/Views/activite/formulaire_activite.php
+++ app/Views/activite/formulaire_activite.php
@@ -1,18 +1,22 @@
-<h2>Saisie d'Activités</h2>
+<div class="container-fluid">
+    <h2 class='mt-2 text-center'>Saisie d'Activités</h2>
+    <hr />
 
-<?php if (session()->getFlashdata('success')): ?>
-    <div class="alert alert-success"><?= session()->getFlashdata('success') ?></div>
-<?php endif; ?>
+    <?php if (session()->getFlashdata('success')): ?>
+        <div class="alert alert-success"><?= session()->getFlashdata('success') ?></div>
+    <?php endif; ?>
 
-<?php form_open('activite/formulaire_activite') ?>
-
-<div class="form-group">
-    <?php form_label('Nom de l\'Activité', 'nom') ?>
-    <?php form_input(['name' => 'nom', 'id' => 'nom', 'class' => 'form-control', 'required' => 'required']) ?>
+    <?php form_open('Activite/formulaire_activite') ?>
+    <div class="mb-3">
+        <?php echo form_label('Nom de l\'Activité', 'nom') ?>
+        <?php echo form_input(['name' => 'nom', 'id' => 'nom', 'class' => 'form-control', 'required' => 'required']) ?>
+    </div>
+    <div class="mb-3">
+        <?php echo form_label('Calories brûlées par minute', 'calories') ?>
+        <?php echo form_input(['name' => 'calories', 'id' => 'calories', 'type' => 'number', 'class' => 'form-control', 'required' => 'required']) ?>
+    </div>
+    <div class="mb-3">
+        <?php echo form_submit(['value' => 'Ajouter Activité', 'class' => 'btn btn-primary']) ?>
+        <?php echo form_close() ?>
+    </div>
 </div>
-<div class="form-group">
-    <?php form_label('Calories brûlées par minute', 'calories') ?>
-    <?php form_input(['name' => 'calories', 'id' => 'calories', 'type' => 'number', 'class' => 'form-control', 'required' => 'required']) ?>
-</div>
-<?php form_submit(['value' => 'Ajouter Activité', 'class' => 'btn btn-primary']) ?>
-<?php form_close() ?>
