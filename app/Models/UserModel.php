86c43eeda3960583128f4e970f0b43c2ba86fdb3
Arnold Jabea <aroljabea71@gmail.com>
2024-09-26 07:24:58 +0200

    update new model(user)

---
+++ app/Models/UserModel.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UserModel extends Model
+{
+    protected $table      = 'users';
+    protected $primaryKey = 'id';
+
+    protected $allowedFields = [
+        'poids',
+        'taille',
+        'sexe',
+        'niveau_activite',
+        'age',
+    ];
+
+    protected $useTimestamps = true;
+    protected $createdField  = 'created_at';
+}
